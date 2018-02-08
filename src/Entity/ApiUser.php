c5de7f808a31fa5ec0400265612e4b23b83da664
jmclosas <miguel@closas.ch>
2018-02-08 09:52:50 +0100

    WebService REST implementiert

---
+++ src/Entity/ApiUser.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="api_user")
+ * @ORM\Entity(repositoryClass="App\Repository\ApiUser")
+ */
+class ApiUser implements UserInterface {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $apiKey;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 6,
+     *      max = 8,
+     *      minMessage = "Your first name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
+     * )
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean", options={"default":true})
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
+
+    public function __construct() {
+        $this->setUpdatedAt(new \DateTime());
+        if ($this->getCreatedAt() == null) {
+            $this->setCreatedAt(new \DateTime());
+        }
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     *
+     * @return sting
+     */
+    public function getSalt() {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getRoles() {
+        return array('ROLE_USER');
+    }
+
+    /**
+     * erase Credentials
+     */
+    public function eraseCredentials() {
+
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getUsername() {
+        return $this->username;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setApiKey($apiKey) {
+        $this->apiKey = $apiKey;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getApiKey() {
+        return $this->apiKey;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username) {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getPassword() {
+        return $this->password;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password) {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt() {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updatetAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return User
+     */
+    public function setCreatedAt($createdAt) {
+        $this->created_at = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatetAt
+     *
+     * @return $updated
+     */
+    public function getUpdatedAt() {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set updatetAt
+     *
+     * @param \DateTime $updatetAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatetAt) {
+        $this->updated_at = $updatetAt;
+
+        return $this;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return User
+     */
+    public function setIsActive($isActive) {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean
+     */
+    public function getIsActive() {
+        return $this->isActive;
+    }
+
+    /**
+     * Get isEnabled
+     *
+     * @return boolean
+     */
+    public function isEnabled() {
+        return $this->isActive;
+    }
+
+}
