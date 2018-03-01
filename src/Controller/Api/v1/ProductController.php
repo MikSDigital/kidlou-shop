f6b353ad18ecf0773277c9f85b27f9d9a60e72f5
jmclosas <miguel@closas.ch>
2018-03-01 08:19:13 +0100

    product json

--- src/Controller/Api/v1/ProductController.php
+++ src/Controller/Api/v1/ProductController.php
@@ -34,11 +34,14 @@ class ProductController extends Controller {
      * @Method("GET")
      */
     public function showAllProducts(Request $request) {
+        $lang = $request->query->get('lang');
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-                "SELECT p.id, p.sku, p.url_key, p.status
+                "SELECT p.id, p.sku, pd.name, p.status
                     FROM App\Entity\Product p
                     INNER JOIN App\Entity\Product\Typ pt WITH p.typ = pt.id AND pt.short_name = 'SIP'
+                    INNER JOIN App\Entity\Product\Description pd WITH p.id = pd.product
+                    INNER JOIN App\Entity\Language l WITH pd.lang = l.id AND l.short_name = '" . $lang . "'
                 "
         );
         $products = $query->getArrayResult();
@@ -58,7 +61,24 @@ class ProductController extends Controller {
      * @Method("GET")
      */
     public function showAction($id, Request $request) {
-
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+                "SELECT p.id, p.sku, p.status, pd.name, pd.short_text, pd.long_text, pd.indicies, pd.accessoires, pr.value, l.short_name
+                    FROM App\Entity\Product p
+                    INNER JOIN App\Entity\Product\Typ pt WITH p.typ = pt.id AND pt.short_name = 'SIP' AND p.id = {$id}
+                    INNER JOIN App\Entity\Product\Description pd WITH p.id = pd.product AND pd.lang IS NOT NULL
+                    INNER JOIN App\Entity\Language l WITH pd.lang = l.id
+                    INNER JOIN App\Entity\Price pr WITH p.id = pr.product
+                    ORDER BY pd.lang ASC
+                "
+        );
+        $products = $query->getArrayResult();
+        $response = new Response();
+        $response->setContent(json_encode($products));
+        $response->headers->set('Content-Type', 'application/json');
+        // Allow all websites
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 
     /**
