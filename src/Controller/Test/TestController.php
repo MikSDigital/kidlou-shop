2b4713e6f5ae71b79e5998ada66a05a9cb6a76f0
jmclosas <miguel@closas.ch>
2018-03-15 15:58:58 +0100

    Paypal Token login implementiert

--- src/Controller/Test/TestController.php
+++ src/Controller/Test/TestController.php
@@ -25,18 +25,27 @@ class TestController extends Controller {
      */
     public function tokenAction() {
         $api_endpoint = "https://api.sandbox.paypal.com/v1/oauth2/token";
+        $clientId = "AbWC1ORqKB2pksz3s5gEDASVugIzv0MuF6m3fySR4ZZFOkPZRwGDNBx0l8zR31tAaMWoX0fdElZKKWPE";
+        $secretKey = "EKL4JF0xTutP3Ldrdc3C86juGBLLyqRY79P4zZ4fO7D3Mtf3Oppv6-28CcrHCDYDIu7s1fYDhN65OlEa";
         // Set the curl parameters.
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Accept: application/json',
-            'Accept-Language: de_DE',
-                )
-        );
-        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
-            'AbWC1ORqKB2pksz3s5gEDASVugIzv0MuF6m3fySR4ZZFOkPZRwGDNBx0l8zR31tAaMWoX0fdElZKKWPE:EKL4JF0xTutP3Ldrdc3C86juGBLLyqRY79P4zZ4fO7D3Mtf3Oppv6-28CcrHCDYDIu7s1fYDhN65OlEa',
-            'grant_type=client_credentials',
-                )
-        );
+        curl_setopt($ch, CURLOPT_URL, $api_endpoint);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $clientId . ":" . $secretKey);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+
+        // Get response from the server.
+        $result = curl_exec($ch);
+
+        if (!$result) {
+            exit("_ failed: " . curl_error($ch) . '(' . curl_errno($ch) . ')');
+        }
+        $json = json_decode($result);
+        print_r($json->access_token);
+        exit;
     }
 
 }
