3500a07547b83c21afee5826d2f88fb62cac3754
jmclosas <miguel@closas.ch>
2018-05-16 18:30:19 +0200

    Anpassungen admin product

--- src/Controller/Api/v1/UserController.php
+++ src/Controller/Api/v1/UserController.php
@@ -9,7 +9,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Entity\User;
 
 /**
  * @Route("/v1")
